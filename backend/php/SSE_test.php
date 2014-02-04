e849d782feba3702fa2ed7f79e229713053a7fa1
Willi <willi.sylvester@web.de>
2014-02-04 20:05:07 +0100

    add background-image add ActiveRecord-testclass add fonts add layout and style of the main page, also add docs-dir with mock-up image add styleguide.txt delete old and obsolete files fixed #38  fixed #39

--- backend/php/SSE_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/event-stream');
-header('Cache-Control: no-cache');
-header('Connection: keep-alive');
-
-$time = date('D, d M Y H:i');
-echo "data: {$time}\n\n";
-flush();
-sleep(1);
