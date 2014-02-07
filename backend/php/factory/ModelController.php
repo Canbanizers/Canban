315d37671b77f86d7d3f380174088259c117c275
janz93 <jan.zaydowicz@googlemail.com>
2014-02-07 18:18:00 +0100

    Change request handling in the backend. added retrieveByCredentials function to see, if a user is registered or not.

---
+++ backend/php/factory/ModelController.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class ModelController {
+
+	public function __construct() {
+		$this->execute();
+	}
+
+	private function execute() {
+
+		$path_to_class = '\models\\';
+		$classname = '';
+
+		if (!$_REQUEST['table']) {
+			die("Invalid Modelclass.");
+		} else {
+			switch ($_REQUEST['table']) {
+				case 'Tickets':
+					$classname .= $_REQUEST['table'];
+					break;
+				default:
+					die("Invalid Modelclass");
+
+			}
+		}
+
+		$classpath = $classname;
+
+		$include_path = getcwd().$path_to_class.$classname.'.php';
+		include($include_path);
+		$class = new $classpath;
+
+		switch ($_REQUEST['action']) {
+			case 'save':
+				$class->createNewTicket();
+				break;
+			default:
+				die("Function not found. Use the same name for funtions and actions");
+		}
+	}
+}
--- client/app/models/user.js
+++ client/app/models/user.js
@@ -1,41 +1,39 @@
+'use strict';
 App.User = DS.Model.extend({
-	//a user can have many groups
-	group: DS.hasMany('group', { async: true }),
-	firstName: DS.attr('string'),
-	lastName: DS.attr('string'),
-	userName: DS.attr('string'),
-	password: DS.attr('number'),
-	email: DS.attr('string'),
-	lastChanged: DS.attr('date'),
-
-	fullName: function(){
-		return this.get('firstName')+' '+this.get('lastName');
-	}.property('firstName', 'lastName')
-
-	//....have to be completed
+	email : DS.attr('string'),
+	firstname : DS.attr('string'),
+	lastname : DS.attr('string'),
+	lastlogin : DS.attr('string'),
+	password : DS.attr('string'),
+	authtoken : DS.attr('string')
 });
-
-
 
 App.User.FIXTURES = [
 	{
-		id: 1,
-		group: [1, 2],
-		firstName: "Eric",
-		lastName: "Cartman",
-		userName: "Coone",
-		email: "cartman@southpark.com",
-		password: "fatass",
-		lastChanged: "Do, 10 Apr 2014 10:10:10 GMT"
+		id : 1,
+		email : 'john@smith.de',
+		firstname : 'John',
+		lastname : 'Smith',
+		lastlogin : 'Learn Ember.js',
+		password : '0815',
+		authtoken : 'AbCdE'
 	},
 	{
-		id: 2,
-		group: [1],
-		firstName: "Kenny",
-		lastName: "McCormick",
-		userName: "Mysterion",
-		email: "kenny@southpark.com",
-		password: "they_killed_kenny",
-		lastChanged: "Do, 10 Apr 2014 11:11:11 GMT"
+		id : 2,
+		email : 'james@macin.de',
+		firstname : 'James',
+		lastname : 'Macin',
+		lastlogin : 'Learn Ember.js',
+		password : 'abc',
+		authtoken : 'ABCDE'
+	},
+	{
+		id : 3,
+		email: 'donald@trump.de',
+		firstname : 'Donald',
+		lastname : 'Trump',
+		lastlogin : 'Learn Ember.js',
+		password : '1234',
+		authtoken : 'abcde'
 	}
 ];
