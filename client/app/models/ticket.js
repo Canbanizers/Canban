376ad289d698ad2d7a5c3137e7323732f4ad4cab
PinkPanther <pinkpanth3r@online.de>
2014-02-25 17:36:08 +0100

    Updated ticket.js to have the same properties as the database table column names plus some useful computed properties

--- client/app/models/ticket.js
+++ client/app/models/ticket.js
@@ -1,23 +1,107 @@
 App.Ticket = DS.Model.extend({
 	title : DS.attr('string'),
-	isDone: DS.attr('boolean')
+	state: DS.attr('integer'),
+	content: DS.attr('string'),
+	priority: DS.attr('integer'),
+	creation_date: DS.attr('date'),
+	last_modify_date: DS.attr('date'),
+	isImportant: function() {
+		return this.get('priority') == 1;
+	}.property('priority'),
+	isTodo: function() {
+		return this.get('state') == 1;
+	}.property('state'),
+	isDoing: function() {
+		return this.get('state') == 2;
+	}.property('state'),
+	isDone: function() {
+		return this.get('state') == 3;
+	}.property('state')
 });
+
 
 
 App.Ticket.FIXTURES = [
 	{
 		id    : 1,
+		state : 3,
 		title : 'Learn Ember.js',
-		isDone: true
+		content: 'Learn Ember.js',
+		priority: 1,
+		creation_date: new Date(),
+		last_modify_date:  new Date()
+
 	},
 	{
 		id    : 2,
-		title : '...',
-		isDone: false
+		state : 2,
+		title : 'This is a important Ticket',
+		content: '...',
+		priority: 1,
+		creation_date: new Date(),
+		last_modify_date: null
 	},
 	{
 		id    : 3,
+		state : 1,
 		title : 'Profit!',
-		isDone: false
+		content: 'Profit',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: null
+	},
+	{
+		id    : 4,
+		state : 1,
+		title : '...',
+		content: '...',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: null
+	},
+	{
+		id    : 5,
+		state : 1,
+		title : '...',
+		content: '...',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: null
+	},
+	{
+		id    : 6,
+		state : 1,
+		title : '...',
+		content: '...',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: null
+	},
+	{
+		id    : 7,
+		state : 2,
+		title : '...',
+		content: '...',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: new Date()
+	},
+	{
+		id    : 8,
+		state : 2,
+		title : '...',
+		content: '...',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: null
+	},
+	{
+		id    : 9,
+		state : 3,
+		title : '...',
+		content: '...',
+		priority: 0,
+		creation_date: new Date(),
+		last_modify_date: null
 	}
 ];
