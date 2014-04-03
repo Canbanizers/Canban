f27c26d25f86ed16d3a5dbf402817180beef0c2a
Willi <willi.sylvester@web.de>
2014-04-03 19:10:11 +0200

    implement CredentialsReader #75 fixed

--- server/factory/utils/CredentialsReader.php
+++ server/factory/utils/CredentialsReader.php
@@ -1,8 +1,18 @@
 <?php
 
+require_once getcwd().DIRECTORY_SEPARATOR.'exceptions'.DIRECTORY_SEPARATOR.'AbstractException.php';
+require_once getcwd().DIRECTORY_SEPARATOR.'exceptions'.DIRECTORY_SEPARATOR.'FileNotFoundException.php';
+
+class UndefinedOffsetException extends AbstractException {
+
+	public function __construct() {
+		$this->setStatusCode(500);
+	}
+}
+
 class CredentialsReader {
 
-	const PATH_TO_XML = '..\xml\db_credentials.xml';
+	const PATH_TO_XML = '..\xml';
 	const PROTOCOL = 'mysql';
 	const SERVER = 'localhost';
 	const DB_NAME = 'canban';
@@ -10,22 +20,37 @@ class CredentialsReader {
 	private $db_password = '';
 
 	private function setDBCredentials() {
-		$xml = simplexml_load_file(__DIR__.'/'.self::PATH_TO_XML);
+
+		$full_path_to_xml = __DIR__.'/'.self::PATH_TO_XML.'\db_credentials.xml';
+
+		if (!file_exists($full_path_to_xml)) {
+			$fnf_e = new FileNotFoundException();
+			$fnf_e->setMessage("Config-File db_credentials.xml in {$full_path_to_xml} not found");
+			throw $fnf_e;
+		}
+
+		$xml = simplexml_load_file($full_path_to_xml);
 
 		$xpaths = array(
 			'password' => '/credentials/password/db/text()',
 			'login'    => '/credentials/login/db/text()'
 		);
 
-		$password = $xml->xpath($xpaths['password']);
-		$this->db_password = (string) $password[0];
 
+		$password = $xml->xpath($xpaths['password']);
 		$login = $xml->xpath($xpaths['login']);
+
+		$this->db_password = (string) $password[0];
 		$this->db_login = (string) $login[0];
 	}
 
 	public function getSqlConnectionString() {
-		$this->setDBCredentials();
+		try {
+			$this->setDBCredentials();
+		} catch (FileNotFoundException $fnf_e) {
+			//TODO: return ist böse weil der wert für db-connection genutzt wird
+			return $fnf_e->getMessage();
+		}
 
 		return
 			self::PROTOCOL.'://'.trim($this->db_login).':'.trim($this->db_password).'@'.self::SERVER.'/'.self::DB_NAME;
