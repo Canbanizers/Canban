0f539aacaecfea4f4613a0bc65bdcf5e69de6113
Willi Sylvester <sylvester@traum-ferienwohnungen.de>
2014-04-10 18:26:33 +0200

    add basic group- and usermodel #76

--- client/app/models/user.js
+++ client/app/models/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict';
 App.User = DS.Model.extend({
 	email : DS.attr('string'),
@@ -37,3 +38,32 @@ App.User.FIXTURES = [
 		authtoken : 'abcde'
 	}
 ];
+=======
+App.User = DS.Model.extend({
+	//a user can have many groups
+	group: DS.hasMany('group', { async: true }),
+	name : DS.attr('string'),
+	password: DS.attr('number'),
+	email: DS.attr('email')
+	//....have to be completed
+});
+
+
+
+App.User.FIXTURES = [
+	{
+		id: 1,
+		group: [1, 2],
+		name: "Eric Cartman",
+		password: "fatass",
+		email: "cartman@southpark.com"
+	},
+	{
+		id: 2,
+		group: [1],
+		name: "Kenny McCormick",
+		password: "they_killed_kenny",
+		email: "kenny@southpark.com"
+	}
+];
+>>>>>>> add basic group- and usermodel #76
