4a6c5b8b131bfcf9e2882ad95f45683c9338f13f
Willi Sylvester <sylvester@traum-ferienwohnungen.de>
2014-04-10 18:25:56 +0200

    add route for groupmanagement #76

--- client/app/app.js
+++ client/app/app.js
@@ -8,8 +8,14 @@ App.ApplicationAdapter = DS.FixtureAdapter.extend();
 //	namespace: 'backend/php/resttest.php'
 //});
 
-App.Router.map(function() {
-	this.resource('private_canban', { path: '/' }, function() {
+App.Router.map(function () {
+	this.resource('private_canban', { path: '/' }, function () {
 		this.resource('board', {path: '/board/:board_id'}, function(){});
+		this.resource('groups', function(){
+			this.resource('group', {path: '/group/:group_id'}, function(){
+				this.route('edit');
+			});
+			this.route('create');
+		});
 	});
 });
