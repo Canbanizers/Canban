41e91daa549c4718fa39b3021686409ce416f6a0
PinkPanth3r <pinkpanth3r@online.de>
2014-04-13 12:59:42 +0200

    Added actions to board-menu and ticket-comps in board.hbs to catch the thrown actions for ticket creation and saving tickets.  Also added the actions for cration and saving purpose to the board_controller. I will explain it later in comment on the commit #85

--- client/app/controllers/board_controller.js
+++ client/app/controllers/board_controller.js
@@ -1,6 +1,8 @@
 'use strict';
 App.BoardController = Ember.ObjectController.extend({
 
+	dummyTicket: null,
+
 	getTodo: function() {
 		return this.get('tickets').filter(function(ticket) {
 			return ticket.get('isTodo');
@@ -20,8 +22,57 @@ App.BoardController = Ember.ObjectController.extend({
 	}.property('tickets.@each.isDone'),
 
 	actions: {
-		switchBoard: function(board) {
+		switchBoard      : function(board) {
 			this.transitionToRoute('board', board.id);
+		},
+		showCreateTicket : function() {
+			this.send('addDummyTicket');
+		},
+		addDummyTicket   : function(state) {
+			console.log("BoardController.addDummyTicket()");
+			var board = this.get('model');
+			console.log(board);
+			var dummyTicket = this.store.createRecord('ticket', {
+				board           : board,
+				state           : 1,
+				title           : '',
+				content         : '',
+				ticketNr        : 500,
+				priority        : 0,
+				creation_date   : null,
+				last_modify_date: null
+			});
+			this.set('dummyTicket', dummyTicket);
+			var tickets = board.get('tickets');
+			tickets.pushObject(dummyTicket);
+		},
+		saveTicket       : function(ticket) {
+			console.log('BoardShow: Received!');
+			if (arguments.length === 1) {
+				console.log('BoardShow: Sending update!');
+				this.send('updateTicket', ticket);
+			} else {
+				console.log('BoardShow: Sending save!');
+				this.send('saveDummyTicket');
+			}
+		},
+		saveDummyTicket  : function() {
+			console.log('Board: Receiving save!');
+			var ticket = this.get('dummyTicket');
+			console.log("dummyTicket");
+			console.log(ticket);
+			ticket.save();
+			ticket.get('board').save();
+		},
+		removeDummyTicket: function() {
+			var dummyTicket = this.get('dummyTicket');
+			var board = this.get('model');
+			var tickets = board.get('tickets');
+			tickets.removeObject(dummyTicket);
+			this.store.deleteRecord(dummyTicket);
+		},
+		updateTicket     : function(ticket) {
+			ticket.save();
 		}
 	}
 });
