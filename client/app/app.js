188dd0c41d683dc3fdcf6f9c5e7a88854773115d
PinkPanth3r <pinkpanth3r@online.de>
2014-04-19 12:10:45 +0200

    Did some fixes in app.js for our ls_rest_adapter implementation fixes #86

--- client/app/app.js
+++ client/app/app.js
@@ -1,30 +1,11 @@
 'use strict';
-window.App = Ember.Application.create({
-	//for debugging
-	LOG_TRANSITIONS: true
-});
+window.App = Ember.Application.create({});
 
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
-
-//App.ApplicationAdapter = DS.RESTAdapter.extend({
-//	host: 'http://localhost/canban',
-//	namespace: 'backend/php/resttest.php'
-//});
-
-App.Router.map(function () {
-	this.resource('private_canban', { path: '/' }, function () {
+App.Router.map(function() {
+	this.resource('private_canban', { path: '/' }, function() {
 		this.resource('board', {path: '/board/:board_id'}, function(){});
-
-		this.resource('groups', function(){
-			this.resource('group', {path: '/:group_id'}, function(){
-				this.route('edit');
-			});
-			this.route('create');
-		});
-
-		this.resource('user', { path:'/user/:user_id' }, function(){
-		});
 	});
+	this.resource('user', { path:'/user/:user_id' }, function(){
 });
 
 App.ApplicationAdapter = DS.LSRESTAdapter.extend({
@@ -79,4 +60,3 @@ DS.JSONSerializer.reopen({
 	}
 
 });
-
