e8b9737995c3fa0907bdb14c181334604ca2afbc
Willi Sylvester <sylvester@traum-ferienwohnungen.de>
2014-05-05 13:00:42 +0200

    reintegrate RestAdapter from master

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,11 @@
 'use strict';
 window.App = Ember.Application.create({
 	//for debugging
-	LOG_TRANSITIONS: true
+	LOG_TRANSITIONS: true,
+	LOG_TRANSITIONS_INTERNAL: true
 });
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
-
-//App.ApplicationAdapter = DS.RESTAdapter.extend({
-//	host: 'http://localhost/canban',
-//	namespace: 'backend/php/resttest.php'
-//});
 
 App.Router.map(function () {
 	this.resource('private_canban', { path: '/' }, function () {
@@ -25,4 +21,52 @@ App.Router.map(function () {
 		this.resource('user', { path:'/user/:user_id' }, function(){
 		});
 	});
+});
+
+
+App.ApplicationSerializer = DS.JSONSerializer.extend({});
+
+DS.JSONSerializer.reopen({
+	/**
+	 * Have to do this to enable arrays of ids to be written in parent models
+	 * with 'hasMany" relationships
+	 */
+	serializeHasMany: function(record, json, relationship) {
+		var key = relationship.key;
+
+		var relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
+
+		if (relationshipType === 'manyToNone' || relationshipType === 'manyToMany' ||
+			relationshipType === 'manyToOne') {
+			json[key] = Ember.get(record, key).mapBy('id');
+		}
+	},
+
+	/**
+	 * Copied from DS.RESTAdapter. This is needed for our own LSRESTAdapter implementation
+	 *
+	 You can use this method to customize the root keys serialized into the JSON.
+	 By default the REST Serializer sends camelized root keys.
+	 For example, your server may expect underscored root objects.
+
+	 ```js
+	 App.ApplicationSerializer = DS.RESTSerializer.extend({
+		serializeIntoHash: function(data, type, record, options) {
+			var root = Ember.String.decamelize(type.typeKey);
+			data[root] = this.serialize(record, options);
+		}
+	});
+	 ```
+
+	 @method serializeIntoHash
+	 @param {Object} hash
+	 @param {subclass of DS.Model} type
+	 @param {DS.Model} record
+	 @param {Object} options
+	 */
+	serializeIntoHash: function(hash, type, record, options) {
+		var root = Ember.String.camelize(type.typeKey);
+		hash[root] = this.serialize(record, options);
+	}
+
 });
