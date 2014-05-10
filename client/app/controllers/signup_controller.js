7ab497e75979a16e5d1e3e08c2becaf0c87f68ff
janz93 <jan.zaydowicz@googlemail.com>
2014-05-10 02:52:22 +0200

    check if user was saved successfully to the database

--- client/app/controllers/signup_controller.js
+++ client/app/controllers/signup_controller.js
@@ -3,7 +3,13 @@ App.SignupController = Ember.ObjectController.extend({
 	actions: {
 		saveUser: function() {
 			var user = this.get('model');
+			/**
+			 * @TODO password will be saved plain into the database, isVaild returns also true on 500 error
+			 */
 			user.save();
+			if(user.get('isValid')) {
+
+			}
 		}
 	}
 });
