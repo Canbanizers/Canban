9703290b61ccd77481428d8111c1b59494122e1c
PinkPanth3r <pinkpanth3r@online.de>
2014-05-18 15:33:36 +0200

    added App.TicketAdapter to app.js as example for catching server errors #60

--- client/app/app.js
+++ client/app/app.js
@@ -14,6 +14,24 @@ App.ApplicationAdapter = DS.LSRESTAdapter.extend({
 	lsnamespace: 'private_canban'
 });
 
+App.TicketAdapter = App.ApplicationAdapter.extend({
+
+	ajaxError: function(jqXHR) {
+		var error = this._super(jqXHR);
+		if (jqXHR && jqXHR.status === 500) {
+			var errorArray = jqXHR.responseJSON.error.split(',');
+			return {
+				serverError : 500,
+				sqlState    : errorArray[0],
+				sqlError    : errorArray[1],
+				errorMessage: errorArray[2]
+			}
+		} else {
+			return error;
+		}
+	}
+});
+
 App.ApplicationSerializer = DS.JSONSerializer.extend({});
 
 DS.JSONSerializer.reopen({
