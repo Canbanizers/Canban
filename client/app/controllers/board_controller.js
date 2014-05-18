595a93ebc9f6611a1bec5534df5cb4fbc579f738
PinkPanth3r <pinkpanth3r@online.de>
2014-05-18 15:32:22 +0200

    created separate functions for ticket creation and deletion and added an error handling example to createTicket. updated board.hbs, so the new functions get called #60

--- client/app/controllers/board_controller.js
+++ client/app/controllers/board_controller.js
@@ -43,16 +43,47 @@ App.BoardController = Ember.ObjectController.extend({
 			var tickets = board.get('tickets');
 			tickets.pushObject(dummyTicket);
 		},
-		saveTicket      : function(ticket) {
+		createTicket    : function(ticket) {
+			ticket.set('creation_date', moment().format(window.timestampFormat));
+			var success = function(resp) {
+			};
+			var secondError = function(resp) {
+				var serverError = -1, sqlState = -1, sqlError = -1, errorMessage = '';
+				if (resp.hasOwnProperty('serverError')) {
+					console.log('hasOwnProperty serverError');
+					serverError = resp['serverError'];
+				} else {
+					console.log('hasNoOwnProperty serverError');
+				}
+				if (resp.hasOwnProperty('sqlState')) {
+					sqlState = resp['sqlState'];
+				}
+				if (resp.hasOwnProperty('sqlError')) {
+					sqlError = resp['sqlError'];
+				}
+				if (resp.hasOwnProperty('errorMessage')) {
+					errorMessage = resp['errorMessage'];
+				}
+				alert("Sorry, we have internal server errors and can't save your ticket at this moment.\n" +
+					  "It will be gone on next page reload, so be sure to save your ticket content in a local file.\n" +
+					  "\n" + "serverError: " + serverError + "\n" + "sqlState: " + sqlState + "\n" + "sqlError: " +
+					  sqlError + "\n" + "errorMessage: " + errorMessage);
+			};
+			var error = function(resp) {
+				ticket.save().then(success, secondError);
+			};
+			ticket.save().then(success, error);
+		},
+
+		updateTicket: function(ticket) {
+			ticket.set('last_modify_date', moment().format(window.timestampFormat));
 			ticket.save();
-			//FIXME At the moment we need to save the board on every edit on its tickets, or the localstorage data will be corrupted. I will try to fix this soon.
-			ticket.get('board').save();
+
 		},
 
 		deleteTicket: function(ticket) {
 			ticket.deleteRecord();
 			ticket.save();
-			this.get('model').save();
 		}
 	}
 });
