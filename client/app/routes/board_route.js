10afa9a8165727beca17718137974390a8401d75
PinkPanth3r <pinkpanth3r@online.de>
2014-05-18 15:28:58 +0200

    Updated private_canban_routes model function to find all tickets and boards and return their quantity as model properties and moved the transitioning to the afterModel hook. Based on this I have refactored the BoardRoute to create a new board if the returned values are 0 #60

--- client/app/routes/board_route.js
+++ client/app/routes/board_route.js
@@ -1,15 +1,15 @@
 App.BoardRoute = Ember.Route.extend({
 	beforeModel: function() {
 		var self = this;
-		var boards = this.store.find('board');
-		return Ember.RSVP.resolve(boards.then(function(boards) {
-			if (!(boards.content.length > 0)) {
-				var board = self.store.createRecord('board', {
-					name: "Personal Board"
-				});
-				return board.save();
-			}
-		}));
+		var privateCanban = this.controllerFor('private_canban');
+		if (privateCanban.get('model') && !(privateCanban.get('boardCount') > 0)) {
+			var board = self.store.createRecord('board', {
+				name: "Personal Board"
+			});
+			return board.save();
+		} else {
+			return null;
+		}
 	},
 	model      : function(params) {
 		var board = this.store.find('board', {name: params.board_name}).then(function(boards) {
--- client/app/routes/private_canban_route.js
+++ client/app/routes/private_canban_route.js
@@ -1,11 +1,23 @@
-var logedIn = true;
-
 App.PrivateCanbanRoute = Ember.Route.extend({
-	beforeModel: function () {
-		if (logedIn) {
-			this.transitionTo('board', 1);
+	loggedIn  : true,
+	model     : function() {
+		var self = this;
+		if (this.get('loggedIn')) {
+			return Ember.RSVP.resolve(self.store.find('ticket').then(function(ticketArray) {
+				return self.store.find('board').then(function(boardArray) {
+					return {
+						boardCount : boardArray.content.length,
+						ticketCount: ticketArray.content.length
+					};
+				});
+			}));
 		} else {
-
+			return null;
+		}
+	},
+	afterModel: function(model) {
+		if (this.get('loggedIn')) {
+			this.transitionTo('board', 'Personal Board');
 		}
 	}
 });
