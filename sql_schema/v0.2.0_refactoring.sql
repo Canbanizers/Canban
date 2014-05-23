550e559f32af12bb7c844ad6acac447986420c99
janz93 <jan.zaydowicz@googlemail.com>
2014-05-23 10:56:04 +0200

    add null into user.last_login column to add user from the registration

--- sql_schema/v0.2.0_refactoring.sql
+++ sql_schema/v0.2.0_refactoring.sql
@@ -1,5 +1,6 @@
 USE `canban`;
 
+#remove unneeded tables
 DROP TABLE IF EXISTS `userhasinvitation`;
 DROP TABLE IF EXISTS `invitations`;
 DROP TABLE IF EXISTS `userhasgrouproles`;
@@ -9,6 +10,7 @@ DROP TABLE IF EXISTS `grouphasuser`;
 DROP TABLE IF EXISTS `grouproles`;
 DROP TABLE IF EXISTS `groups`;
 
+#empty all tables
 TRUNCATE TABLE `boardhasticket`;
 TRUNCATE TABLE `userhasboard`;
 
@@ -16,8 +18,12 @@ DELETE From `tickets`;
 DELETE From `users`;
 DELETE From `boards`;
 
+#reset auto_increment
 ALTER TABLE `boardhasticket` AUTO_INCREMENT = 1;
 ALTER TABLE `userhasboard` AUTO_INCREMENT = 1;
 ALTER TABLE `tickets` AUTO_INCREMENT = 1;
 ALTER TABLE `users` AUTO_INCREMENT = 1;
 ALTER TABLE `boards` AUTO_INCREMENT = 1;
+
+#change last_login for registration user to have null in last_login
+ALTER TABLE `users` CHANGE `last_login` `last_login` DATETIME NULL;
