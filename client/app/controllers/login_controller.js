09a9f11da84784ab07521b5ce8c076a0deb5de48
Willi Sylvester <sylvester@traum-ferienwohnungen.de>
2014-05-26 17:37:04 +0200

    implement errorhandling before sending the request #42

--- client/app/controllers/login_controller.js
+++ client/app/controllers/login_controller.js
@@ -1,31 +1,38 @@
 'use strict';
 App.LoginController = Ember.ObjectController.extend({
 	needs: ['validation', 'private_canban'],
+	noChanges: true,
+
 	/**
-	 * set this as the current controller for the validation, the validationcontroller have access to it
+	 * set this as the current controller for the validation and allows the validationcontroller to have access to it
 	 */
 	setController: function() {
 		this.get('controllers.validation').initializeController(this);
 	},
 	validateEmail: function() {
 		this.set('emailError', this.get('controllers.validation').getValue('email'));
-		this.set('emailError', this.get('controllers.validation').isValidEmail('email'));
+		this.set('invalidEmailError', this.get('controllers.validation').isValidEmail('email'));
+		this.set('noChanges', false);
 	},
 	validatePassword: function() {
 		this.set('passwordError', this.get('controllers.validation').getValue('password'));
+		this.set('noChanges', false);
 	},
 	actions: {
 		login: function() {
-			var user = this.store.find('user', {email: this.get('email'), password: this.get('password')});
-			var self = this;
-			console.log(user);
-			user.then(function() {
-				console.log('abcdsad');
-				self.get('controllers.private_canban').user = user;
-				self.transitionTo('private_canban');
-			}, function() {
-				console.log('abc');
-			});
+			if(this.get('controllers.validation').hasErrors() && this.get('noChanges')) {
+				this.set('saveError', true);
+			} else {
+				this.set('saveError', false);
+				var user = this.store.find('user', {email: this.get('email'), password: this.get('password')});
+				var self = this;
+				user.then(function() {
+					//TODO: Transition don't work yet.
+					self.get('controllers.private_canban').user = user;
+					self.transitionToRoute('private_canban');
+				}, function() {
+				});
+			}
 		}
 	}
 });
