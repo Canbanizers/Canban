e366ef47b94fe21b30ae81abbe25c796790616c7
janz93 <jan.zaydowicz@googlemail.com>
2014-05-26 22:05:37 +0200

    move adapter from the app.js into an own directory

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -117,8 +117,7 @@ module.exports = function(grunt) {
 		watch           : {
 			scripts: {
 				files  : ['app/lib/*.js', 'app/*.js', 'app/controllers/*.js', 'app/views/*.js', 'app/routes/*.js',
-						  'app/models/*.js', 'app/css/*.less', 'app/templates/**/*.hbs', 'app/transforms/*.js',
-						  'app/adapters/*.js'],
+						  'app/models/*.js', 'app/css/*.less', 'app/templates/**/*.hbs', 'app/transforms/*.js'],
 				tasks  : ['ember_handlebars', 'concat', 'less'],
 				options: {
 					debounceDelay: 100
