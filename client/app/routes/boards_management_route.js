9b34355d5f128165e1c1f5ff5b78062ba82d764c
PinkPanth3r <pinkpanth3r@online.de>
2014-06-01 18:27:22 +0200

    add boards_route and boards_management_route 52bd8f6 #99

---
+++ client/app/routes/boards_management_route.js
@@ -0,0 +1,32 @@
+'use strict';
+
+App.BoardsManagementRoute = Ember.Route.extend({
+	toRender: null,
+
+	model: function() {
+		return this.store.find('board');
+	},
+
+	actions: {
+		boardInfo: function(board) {
+			var controller = this.controllerFor('board.info');
+			controller.send('changeBoard', board);
+			this.send('renderInfoWithController', controller);
+		},
+
+		boardSettings: function(board) {
+			var controller = this.controllerFor('board.edit');
+			controller.set('model', board);
+			this.send('renderInfoWithController', controller);
+		},
+
+		renderInfoWithController: function(controller) {
+			this.render('board.info', {
+				into      : 'boards.management',
+				outlet    : 'content',
+				controller: controller
+			});
+
+		}
+	}
+});
