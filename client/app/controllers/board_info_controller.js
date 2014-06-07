fbc7d1efea3886313a23c7a2d773fe781957e0ef
PinkPanth3r <pinkpanth3r@online.de>
2014-06-07 13:22:14 +0200

    remove obsolete changeBoard action from board_info_controller.js #99

--- client/app/controllers/board_info_controller.js
+++ client/app/controllers/board_info_controller.js
@@ -1,16 +1,8 @@
 App.BoardInfoController = Ember.ObjectController.extend({
 	needs   : ['boards'],
 	info    : true,
-        
-        hasChildren: function() {
-               return this.get('children.length') > 0;
-        }.property('children.length'),
 
-        
-
-	actions: {
-		changeBoard: function(board) {
-			this.set('model', board);
-		}
-	}
+	hasChildren: function() {
+		return this.get('children.length') > 0;
+	}.property('children.length')
 });
