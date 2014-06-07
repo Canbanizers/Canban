911b1ffbc91e8813c7d1320fbe91484db54d0642
PinkPanth3r <pinkpanth3r@online.de>
2014-06-07 13:25:21 +0200

    board_route.js now implements replacing of underscores to whitespaces to 'decode' the url fix #117 also move switchBoard action from board_show_route.js to board_route.js

--- client/app/routes/board_show_route.js
+++ client/app/routes/board_show_route.js
@@ -1,11 +1,5 @@
 App.BoardShowRoute = Ember.Route.extend({
 	model: function() {
 		return this.modelFor('board');
-	},
-
-	actions: {
-		switchBoard: function(board) {
-			this.send('saveTransition', 'board.show', board.get('name'));
-		}
 	}
 });
